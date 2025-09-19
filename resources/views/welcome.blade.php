a5b56130f71b131fca43184b219e72d0715bd3ca
Gaurav Srivastava <gauravsr17300@gmail.com>
2025-09-20 00:04:06 +0530

    Dashboard fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,10 +34,11 @@
                     @auth
                         <a
                             href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                            class="ms-3 inline-block px-5 py-1.5 bg-white font-bold border border-transparent  dark:hover:border-white rounded-sm text-sm leading-normal"
                         >
                             Dashboard
                         </a>
+                        <a href="{{ route('books.index') }}" class="ms-3 inline-block px-5 py-1.5 bg-white font-bold border border-transparent  dark:hover:border-white rounded-sm text-sm leading-normal">See Books</a>
                     @else
                         <a
                             href="{{ route('login') }}"
